e1b840ea65f10e4814336d14b6ff14d08b4384a2
Lycheejam <lycheejamwow@gmail.com>
2021-01-25 01:17:21 +0900

    :+1: チャンネル通知関連を実装した

--- src/events/index.ts
+++ src/events/index.ts
@@ -21,4 +21,56 @@ export const events = (app: App): void => {
       console.log(error);
     }
   });
+
+  app.event("channel_created", async ({ event, context}) => {
+    try {
+      await app.client.chat.postMessage({
+        token: context.botToken,
+        channel: ,
+        text: `:tada: added new channel : #${event.channel.name} `,
+        link_names: true
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  app.event("channel_rename", async ({ event, context}) => {
+    try {
+      await app.client.chat.postMessage({
+        token: context.botToken,
+        channel: ,
+        text: `:pencil2: rename channel : #${event.channel.name} `,
+        link_names: true
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  app.event("channel_archive", async ({ event, context}) => {
+    try {
+      await app.client.chat.postMessage({
+        token: context.botToken,
+        channel: ,
+        text: `:notebook: archived channel : #${event.channel} `,
+        link_names: true
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  app.event("channel_deleted", async ({ event, context}) => {
+    try {
+      await app.client.chat.postMessage({
+        token: context.botToken,
+        channel: ,
+        text: `:shower: deleted channel : #${event.channel} `,
+        link_names: true
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
 };
