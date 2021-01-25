f5d45f71c7cbc56a083a1736e6c42360385a3804
Lycheejam <lycheejamwow@gmail.com>
2021-01-26 00:51:14 +0900

    :tada: とりあえず動いた

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,73 +1,69 @@
 import { App } from "@slack/bolt";
 
+const EMOJI_NOTIFICATION_CHANNEL: string = process.env.EMOJI_NOTIFICATION_CHANNEL!;
+const NOTIFICATION_CHANNEL: string = process.env.NOTIFICATION_CHANNEL!;
 export const events = (app: App): void => {
-  app.event("emoji_changed", async ({ event, context}) => {
-    var message = '';
-    if (event.subtype == 'add') {
-      message = `:tada: added new emoji : \`:${event.name}:\` :${event.name}:`
-    } else if (event.subtype == 'remove') {
-      message = `:shower: removed emoji : \`:${event.names[0]}:\``
-    } else if (event.subtype == 'rename') {
-      message = `:pencil2: rename emoji alias : \`:${event.old_name}:\` → \`:${event.new_name}:\` :${event.new_name}:`
-    }
 
-    try {
-      await app.client.chat.postMessage({
-        token: context.botToken,
-        channel: ,
-        text: message
-      });
-    } catch (error) {
-      console.log(error);
+  app.event("emoji_changed", async ({ event, context }) => {
+    if (event.subtype === "add") {
+      try {
+        await app.client.chat.postMessage({
+          token: context.botToken,
+          channel: EMOJI_NOTIFICATION_CHANNEL,
+          text: `:tada: added new emoji : \`:${event.name}:\` :${event.name}:`,
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
   });
 
-  app.event("channel_created", async ({ event, context}) => {
+  app.event("channel_created", async ({ event, context }) => {
     try {
       await app.client.chat.postMessage({
         token: context.botToken,
-        channel: ,
+        channel: NOTIFICATION_CHANNEL,
         text: `:tada: added new channel : #${event.channel.name} `,
-        link_names: true
+        link_names: true,
       });
     } catch (error) {
       console.log(error);
     }
   });
 
-  app.event("channel_rename", async ({ event, context}) => {
+  app.event("channel_rename", async ({ event, context }) => {
     try {
       await app.client.chat.postMessage({
         token: context.botToken,
-        channel: ,
+        channel: NOTIFICATION_CHANNEL,
         text: `:pencil2: rename channel : #${event.channel.name} `,
-        link_names: true
+        link_names: true,
       });
     } catch (error) {
       console.log(error);
     }
   });
 
-  app.event("channel_archive", async ({ event, context}) => {
+  app.event("channel_archive", async ({ event, context }) => {
     try {
       await app.client.chat.postMessage({
         token: context.botToken,
-        channel: ,
+        channel: NOTIFICATION_CHANNEL,
         text: `:notebook: archived channel : #${event.channel} `,
-        link_names: true
+        link_names: true,
       });
     } catch (error) {
       console.log(error);
     }
   });
 
-  app.event("channel_deleted", async ({ event, context}) => {
+  app.event("channel_deleted", async ({ event, context }) => {
     try {
       await app.client.chat.postMessage({
         token: context.botToken,
-        channel: ,
+        channel: NOTIFICATION_CHANNEL,
         text: `:shower: deleted channel : #${event.channel} `,
-        link_names: true
+        link_names: true,
       });
     } catch (error) {
       console.log(error);
